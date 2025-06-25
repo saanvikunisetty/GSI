9094346f87e4370b2a5c8c4cfa25ec30c0447b74
Saanvi Kunisetty <4saanvik@gmail.com>
2025-06-24 20:39:34 -0500

    Added Detection of Multi-Select Survey Columns

--- GIS/GIS.py
+++ GIS/GIS.py
@@ -3,4 +3,24 @@ df = pd.read_csv("survey.csv")
 #print(df)
 
 df.columns = [col.split(":")[0].strip() for col in df.columns]
-print(df.columns)
+#print(df.columns)
+
+df = df.drop(columns=["Status", 
+                      "Contact ID", 
+                      "Link Name", 
+                      "URL Variable", 
+                      "Invite Status",
+                      "Survey Batch",
+                      "Email",
+                      "First Name",
+                      "Last Name",
+                      "Start of Survey" 
+                      "Time Started"], 
+                      errors='ignore')
+
+multi_select_cols = [
+    col for col in df.columns 
+    if "please select all" in col.lower() or "please check all" in col.lower()
+]
+
+print("Multi-select columns:", multi_select_cols)
